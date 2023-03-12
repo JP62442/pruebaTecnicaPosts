cf9ab6fb5616d75a656ecee58856eae3880959af
jpdev <jp143dev@gmail.com>
2023-03-11 19:23:45 -0500

    :recycle: perf: Add React Hook Form in the modal for create the Post.

--- src/Pages/Posts/index.jsx
+++ src/Pages/Posts/index.jsx
@@ -1,30 +1,34 @@
 import React, { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+
+import { TextField } from "@mui/material";
 
 import { Header } from "../../components/Header";
 import { ListOfPosts } from "./components/ListOfPost";
 import { Modal } from "./components/Modal";
 
-function Posts() {
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
+import "./styles.css";
 
+function Posts() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleModalClose = () => {
-    setIsModalOpen(false);
-  };
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      title: "",
+      body: "",
+    },
+  });
+
+  const onSubmit = (data) => console.log(data);
 
   const handleModalOpen = () => {
     setIsModalOpen(true);
   };
-  const handleSubmit = () => {
-    const newPost = {
-      title: title,
-      body: body,
-    };
-    setData((prevData) => [...prevData, newPost]);
-    setTitle("");
-    setBody("");
+  const handleModalClose = () => {
     setIsModalOpen(false);
   };
 
@@ -34,20 +38,39 @@ function Posts() {
       <ListOfPosts handleModalOpen={handleModalOpen} />
 
       <Modal isOpen={isModalOpen} onClose={handleModalClose}>
-        <div className="modalContent">
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder="Title"
+        <form className="modalContent" onSubmit={handleSubmit(onSubmit)}>
+          <Controller
+            name="title"
+            rules={{ required: true }}
+            control={control}
+            render={({ field }) => (
+              <TextField
+                label="Título"
+                variant="outlined"
+                error={errors.title ? true : false}
+                {...field}
+              />
+            )}
           />
-          <textarea
-            value={body}
-            onChange={(e) => setBody(e.target.value)}
-            placeholder="Body"
+          {errors.title && <span>Este campo es requerido</span>}
+
+          <Controller
+            name="body"
+            rules={{ required: true }}
+            control={control}
+            render={({ field }) => (
+              <TextField
+                multiline
+                label="Cuerpo"
+                variant="outlined"
+                error={errors.body ? true : false}
+                {...field}
+              />
+            )}
           />
+          {errors.body && <span>Este campo es requerido</span>}
           <button onClick={handleSubmit}>Add</button>
-        </div>
+        </form>
       </Modal>
     </>
   );
