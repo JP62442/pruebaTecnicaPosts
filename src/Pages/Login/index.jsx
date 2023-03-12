e9c86a41276c7a23d9d169b65d2682d4d29ae277
jpdev <jp143dev@gmail.com>
2023-03-12 18:59:57 -0500

    :lipstick: perf: Basic stylized version of the Login, only using MaterialUI.

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,9 +1,49 @@
 import React, { useState } from "react";
+
+import toast, { Toaster } from "react-hot-toast";
+
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useForm, Controller } from "react-hook-form";
-import { TextField } from "@mui/material";
+import { TextField, Button, Box, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+import LockOpenIcon from "@mui/icons-material/LockOpen";
+
+const StyledForm = styled("form")({
+  display: "flex",
+  flexDirection: "column",
+  gap: "20px",
+  maxWidth: "400px",
+  margin: "0 auto",
+});
+
+const StyledInputWrapper = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+});
+
+const StyledLabel = styled("label")({
+  marginBottom: "5px",
+  fontSize: "16px",
+  fontWeight: "bold",
+});
+
+const StyledError = styled("span")({
+  color: "red",
+  fontSize: "14px",
+});
+
+const StyledSubmitButton = styled(Button)({
+  background: "#2196f3",
+  color: "#fff",
+  "&:hover": {
+    background: "#0d8bf2",
+  },
+});
 
 export const Login = () => {
+  const notifyError = () => toast.error("Cuenta invalida, intenta otra vez.");
+
   const {
     control,
     handleSubmit,
@@ -23,57 +63,65 @@ export const Login = () => {
 
     try {
       await signInWithEmailAndPassword(auth, email, password);
-      console.log(data);
     } catch (error) {
       setError(error.message);
+      notifyError();
     }
   };
 
   return (
-    <div>
-      <h1>Iniciar sesión</h1>
-      <form className="modalContent" onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <Controller
-            name="email"
-            rules={{ required: true }}
-            control={control}
-            render={({ field }) => (
-              <TextField
-                id="email"
-                label="Email"
-                variant="outlined"
-                {...field}
-              />
+    <Box height="100vh" textAlign="center">
+      <Box padding="20vh">
+        <LockOpenIcon fontSize="large" />
+        <Typography variant="h5" gutterBottom marginBottom="50px">
+          Iniciar sesión
+        </Typography>
+        <StyledForm onSubmit={handleSubmit(onSubmit)}>
+          <StyledInputWrapper>
+            <StyledLabel htmlFor="email">Email:</StyledLabel>
+            <Controller
+              name="email"
+              rules={{ required: true }}
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  id="email"
+                  label="Email"
+                  variant="outlined"
+                  {...field}
+                />
+              )}
+            />
+            {errors.email && <StyledError>Este campo es requerido</StyledError>}
+          </StyledInputWrapper>
+
+          <StyledInputWrapper>
+            <StyledLabel htmlFor="password">Contraseña:</StyledLabel>
+            <Controller
+              name="password"
+              rules={{ required: true }}
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  id="password"
+                  label="Contraseña"
+                  variant="outlined"
+                  type="password"
+                  {...field}
+                />
+              )}
+            />
+            {errors.password && (
+              <StyledError>Este campo es requerido</StyledError>
             )}
-          />
-          {errors.email && <span>Este campo es requerido</span>}
-        </div>
+          </StyledInputWrapper>
 
-        <div>
-          <label htmlFor="password">Contraseña:</label>
-          <Controller
-            name="password"
-            rules={{ required: true }}
-            control={control}
-            render={({ field }) => (
-              <TextField
-                id="password"
-                label="Contraseña"
-                variant="outlined"
-                type="password"
-                {...field}
-              />
-            )}
-          />
-          {errors.password && <span>Este campo es requerido</span>}
-        </div>
+          <StyledSubmitButton type="submit">Iniciar sesión</StyledSubmitButton>
+        </StyledForm>
 
-        <button type="submit">Iniciar sesión</button>
-      </form>
-
-      {error && <div>{error}</div>}
-    </div>
+        {error && <div>{"Cuenta invalida, intenta otra vez."}</div>}
+        <Toaster position="bottom-center" reverseOrder={false} />
+      </Box>
+    </Box>
   );
 };
